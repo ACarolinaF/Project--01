eb16b7213cdabc9eec2ef75cb9fa1d234a51416e
ACarolinaF <fernandes.carolina.ana@gmail.com>
2022-03-25 04:57:34 +0000

    updating form in createUser

--- client/src/Components/CreateUser/CreateUser.jsx
+++ client/src/Components/CreateUser/CreateUser.jsx
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 import NavBar from "../NavBar/NavBar";
 
 
-export default function CreateUser (){
+export default function CreateUser() {
+
+    let [input, setInput] = useState({
+        name:'',
+        lastName:'',
+        age:'',
+        city:''
+    })
+
+    let handleChange = (e) =>{
+        e.preventDefault();
+        setInput((prev)=>({
+            ...prev, 
+            [e.target.name]: e.target.value})
+        )
+    }
+
+    let handleSubmit = (e) =>{
+        e.preventDefault();
+        //dispatch de uma ação que me encha o estado global dessa informação
+        setInput({
+            name:'',
+            lastName:'',
+            age:'',
+            city:''
+        });
+    }
 
     return (
         <div>
-            <NavBar/>
+            <NavBar />
+            <br />
             CREATE USER
+            <br />
+            <form onSubmit={(e)=>handleSubmit(e)}>
+                <div>
+                    <label>Name</label>
+                    <input type='text' id='name' value={input.name} onChange={(e)=>handleChange(e)}/>
+                </div>
+
+                <div>
+                    <label>Last Name</label>
+                    <input type='text' id='lastName' value={input.lastName} onChange={(e)=>handleChange(e)}/>
+                </div>
+
+                <div>
+                    <label>Age</label>
+                    <input type='text' id='age' value={input.age} onChange={(e)=>handleChange(e)}/>
+                </div>
+
+                <div>
+                    <label>City</label>
+                    <input type='text' id='city' value={input.city} onChange={(e)=>handleChange(e)}/>
+                </div>
+
+                <input type='submit' value='CREATE'/>
+                {/* outra opção para o input seria um botão que quando se faz onClick ele ativa 
+                exatamente a mesma ação - handleSubmit */}
+
+            </form>
+
+
         </div>
     )
 }
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -19,6 +19,8 @@ export default function Home ({name}){
             Then all the home:
             this is home
              - {name}
+
+
         </div>
     )
 }
